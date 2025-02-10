4fc85294e7288645676530ce1378fb8b07680b8e
michellviu <michellviuramirez@gmail.com>
2025-02-10 09:39:55 -0500

    fix: Update protocol version and modify run script to execute IRCClient

--- env.sh
+++ env.sh
@@ -7,7 +7,7 @@
 # 3. SMTP
 # 4. IRC
 
-PROTOCOL=1
+PROTOCOL=4
 
 # Don't modify the next line
 echo "PROTOCOL=${PROTOCOL}" >> "$GITHUB_ENV"
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 # Replace the next shell command with the entrypoint of your solution
 
-echo $@
+python (IRCClient.py) "$@"
